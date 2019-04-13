c7dea27f48b1cd543a220d892e7bf4dbcd428954
Paavo Huhtala <paavo.huhtala@gmail.com>
2019-04-13 13:29:50 +0300

    Rewrite atoms using BehaviorSubject

--- rxjs-atoms/src/Atom.ts
+++ rxjs-atoms/src/Atom.ts
@@ -1,25 +1,11 @@
-import { Observable, Subscriber } from "rxjs";
+import { Observable, BehaviorSubject } from "rxjs";
 import { Lens, ReadLens, EitherLens } from "./Lens";
+import { distinctUntilChanged, map } from "rxjs/operators";
 
 type EitherAtom<T> = AbstractReadAtom<T> | AbstractAtom<T>;
 
 export type AtomValue<A extends EitherAtom<any>> = A extends EitherAtom<infer T>
   ? T
-  : never;
-
-type LensAtomWith<
-  O extends EitherAtom<any>,
-  L extends EitherLens<any, any>
-> = O extends AbstractAtom<infer A>
-  ? L extends Lens<A, infer B>
-    ? AbstractAtom<B>
-    : L extends ReadLens<A, infer B>
-    ? AbstractReadAtom<B>
-    : never
-  : O extends AbstractReadAtom<infer A>
-  ? L extends EitherLens<A, infer B>
-    ? AbstractReadAtom<B>
-    : never
   : never;
 
 export interface AbstractReadAtom<T> extends Observable<T> {
@@ -36,31 +22,21 @@ export interface AbstractAtom<T> extends AbstractReadAtom<T> {
   view<B>(getter: ReadLens<T, B>): AbstractReadAtom<B>;
 }
 
-export class Atom<T> extends Observable<T> implements AbstractAtom<T> {
-  private subscribers: Subscriber<T>[] = [];
-  private currentValue: T;
-
+export class Atom<T> extends BehaviorSubject<T> implements AbstractAtom<T> {
   constructor(value: T) {
-    super(emitter => {
-      this.subscribers.push(emitter);
-      emitter.next(this.currentValue);
-    });
-
-    this.currentValue = value;
+    super(value);
   }
 
   get() {
-    return this.currentValue;
+    return this.getValue();
   }
 
   set(value: T) {
-    this.currentValue = value;
-    this.notify(value);
+    this.next(value);
   }
 
   modify(f: (oldValue: T) => T) {
-    this.set(f(this.currentValue));
-    return this.currentValue;
+    this.set(f(this.getValue()));
   }
 
   view<B>(lens: Lens<T, B>): AbstractAtom<B>;
@@ -72,45 +48,27 @@ export class Atom<T> extends Observable<T> implements AbstractAtom<T> {
       return LensedReadAtom.create(this, lens);
     }
   }
-
-  private notify(x: T) {
-    this.subscribers.forEach(emitter => !emitter.closed && emitter.next(x));
-  }
 }
 
-export class LensedAtom<T> extends Observable<T> implements AbstractAtom<T> {
-  private subscribers: Subscriber<T>[] = [];
+export class LensedAtom<T> extends BehaviorSubject<T>
+  implements AbstractAtom<T> {
   private inner: AbstractAtom<any>;
   private lens: Lens<any, T>;
-  private currentValue: T;
 
   private constructor(atom: AbstractAtom<any>, lens: Lens<any, T>) {
-    super(emitter => {
-      this.subscribers.push(emitter);
-      emitter.next(this.currentValue);
-    });
-
-    this.currentValue = lens.get(atom.get());
+    super(lens.get(atom.get()));
 
     this.inner = atom;
     this.lens = lens;
 
-    this.inner.subscribe({
-      next: x => {
-        const newValue = this.lens.get(x);
-
-        if (this.currentValue !== newValue) {
-          this.currentValue = newValue;
-          this.subscribers.forEach(
-            emitter => !emitter.closed && emitter.next(this.currentValue!)
-          );
-        }
-      }
-    });
+    this.inner
+      .pipe(map(lens._get))
+      .pipe(distinctUntilChanged())
+      .subscribe(this);
   }
 
   get() {
-    return this.currentValue;
+    return this.getValue();
   }
 
   set(value: T) {
@@ -136,48 +94,25 @@ export class LensedAtom<T> extends Observable<T> implements AbstractAtom<T> {
   }
 }
 
-export class LensedReadAtom<T> extends Observable<T>
+export class LensedReadAtom<T> extends BehaviorSubject<T>
   implements AbstractReadAtom<T> {
-  private subscribers: Subscriber<T>[] = [];
   private inner: AbstractReadAtom<any>;
   private lens: ReadLens<any, T>;
-  private currentValue: T;
 
   private constructor(atom: AbstractReadAtom<any>, lens: ReadLens<any, T>) {
-    super(emitter => {
-      this.subscribers.push(emitter);
-      emitter.next(this.currentValue);
-    });
+    super(lens.get(atom.get()));
 
-    this.currentValue = lens.get(atom.get());
     this.inner = atom;
     this.lens = lens;
 
-    this.inner.subscribe({
-      next: x => {
-        if (this.subscribers.length === 0) return;
-        const newValue = this.lens.get(x);
-
-        if (this.currentValue !== newValue) {
-          this.currentValue = newValue;
-          this.subscribers.forEach(
-            emitter => !emitter.closed && emitter.next(this.currentValue)
-          );
-        }
-      }
-    });
-  }
-
-  private forceGet() {
-    return this.lens.get(this.inner.get());
+    this.inner
+      .pipe(map(lens._get))
+      .pipe(distinctUntilChanged())
+      .subscribe(this);
   }
 
   get() {
-    if (this.currentValue === undefined) {
-      this.currentValue = this.forceGet();
-    }
-
-    return this.currentValue;
+    return this.getValue();
   }
 
   view<B>(getter: ReadLens<T, B>): AbstractReadAtom<B> {
--- rxjs-atoms/src/Molecule.ts
+++ rxjs-atoms/src/Molecule.ts
@@ -1,5 +1,5 @@
 import { AbstractReadAtom, AtomValue } from ".";
-import { Observable, combineLatest, Subscriber } from "rxjs";
+import { combineLatest, BehaviorSubject } from "rxjs";
 import { map } from "rxjs/operators";
 import { ReadLens } from "./Lens";
 import { LensedReadAtom } from "./Atom";
@@ -13,52 +13,26 @@ const combinePartsUnsafe = <T extends object>() => (parts: object[]): T => {
 };
 
 export class Molecule<O extends Record<any, AbstractReadAtom<any>>>
-  extends Observable<CombineAtoms<O>>
+  extends BehaviorSubject<CombineAtoms<O>>
   implements AbstractReadAtom<CombineAtoms<O>> {
-  private subscribers: Subscriber<CombineAtoms<O>>[] = [];
-
-  private innerAtoms: O;
-  private combinedObservable: Observable<CombineAtoms<O>>;
-  private combinedValue: CombineAtoms<O>;
-
   constructor(atoms: O) {
-    super(emitter => {
-      this.subscribers.push(emitter);
-      emitter.next(this.combinedValue);
-    });
+    // This is safe, because the subscribe() call below immediately calls next()
+    super(undefined!);
 
-    this.innerAtoms = atoms;
-    this.combinedValue = this.forceGet();
-
-    this.combinedObservable = combineLatest(
+    combineLatest(
       ...Object.entries(atoms).map(([k, obs]) =>
         obs.pipe(map(x => ({ [k]: x })))
       )
-    ).pipe(map(combinePartsUnsafe<CombineAtoms<O>>()));
-
-    this.combinedObservable.subscribe(parts => {
-      this.notify(parts);
-    });
+    )
+      .pipe(map(combinePartsUnsafe<CombineAtoms<O>>()))
+      .subscribe(this);
   }
 
   get(): CombineAtoms<O> {
-    if (this.combinedValue === undefined) {
-      this.combinedValue = this.forceGet();
-    }
-    return this.combinedValue;
-  }
-
-  private forceGet(): CombineAtoms<O> {
-    return combinePartsUnsafe<CombineAtoms<O>>()(
-      Object.entries(this.innerAtoms).map(([k, v]) => ({ [k]: v.get() }))
-    );
+    return this.getValue();
   }
 
   view<B>(getter: ReadLens<CombineAtoms<O>, B>): AbstractReadAtom<B> {
     return LensedReadAtom.create(this, getter);
-  }
-
-  private notify(x: CombineAtoms<O>) {
-    this.subscribers.forEach(emitter => !emitter.closed && emitter.next(x));
   }
 }
